20950846cd392a4b6cef347865640b639a633ee5
Quentin <quentin.bertramo@gmail.com>
2022-12-28 19:49:29 +0100

    Analyse des données: Fusion des données Clients et Immatriculations : rapport

--- Analyse des Données/4Fusion des données Clients et Immatriculations/4_fusion_des_donnees_clients_et_immatriculations.R
+++ Analyse des Données/4Fusion des données Clients et Immatriculations/4_fusion_des_donnees_clients_et_immatriculations.R
@@ -1,23 +1,70 @@
 ## Fusion des données clients et immatriculations
 
-# Chargement des données
-clients1 <- read.csv(dec = ".", file = "Clients_2.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
-clients2 <- read.csv(dec = ".", file = "Clients_10.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
-immatriculations <- read.csv(dec = ".", file = "Immatriculations_predites.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
-
-# Fusion des dataframes clients
-clients <- rbind(clients1, clients2)
-
-# Jointure des dataframe clients et immatriculations
-# en ne considérant que les modalités communes de la variable de jointure
-# https://delladata.fr/fusionner-deux-tableaux-de-donnees-par-leurs-colonnes-les-jointures/
+#--------------------------------------------#
+# INSTALLATION/MAJ DES LIRAIRIES NECESSAIRES #
+#--------------------------------------------#
 install.packages("dplyr")
+
+#--------------------------------------#
+# ACTIVATION DES LIRAIRIES NECESSAIRES #
+#--------------------------------------#
 library(dplyr)
 
-df_ij <- inner_join(clients, immatriculations, by="immatriculation")
+#-------------------------#
+# PREPARATION DES DONNEES #
+#-------------------------#
+# Chargement des données
+clients1 <- read.csv(
+    dec = ".",
+    file = "Clients_2.csv",
+    header = TRUE,
+    sep = ",",
+    stringsAsFactors = FALSE,
+    fileEncoding = "CP1252"
+)
+clients2 <- read.csv(
+    dec = ".",
+    file = "Clients_10.csv",
+    header = TRUE,
+    sep = ",",
+    stringsAsFactors = FALSE,
+    fileEncoding = "CP1252"
+)
+immatriculations <- read.csv(
+    dec = ".",
+    file = "Immatriculations.csv",
+    header = TRUE,
+    sep = ",",
+    stringsAsFactors = FALSE,
+    fileEncoding = "UTF-8"
+)
 
-# Suppression des duplicates
-df_clients_immatriculations <- unique(df_ij)
+#----------------------------#
+# FUSION DES DONNÉES CLIENTS #
+#----------------------------#
+clients <- rbind(clients1, clients2)
 
 # Enregistrement du fichier de resultats au format csv
-write.table(df_ij, file="Clients_immatriculations.csv", sep=",", dec=".", row.names = F)
+write.table(clients, file = "Clients.csv", sep = ",", dec = ".", row.names = F)
+
+clients <- read.csv(
+  dec = ".",
+  file = "Clients.csv",
+  header = TRUE,
+  sep = ",",
+  stringsAsFactors = FALSE,
+  fileEncoding = "UTF-8"
+)
+
+#------------------------------------------------#
+# FUSION DES DONNÉES CLIENTS ET IMMATRICULATIONS #
+#------------------------------------------------#
+# Jointure des dataframe clients et immatriculations en ne considérant que les modalités communes de la variable de jointure
+# https://delladata.fr/fusionner-deux-tableaux-de-donnees-par-leurs-colonnes-les-jointures/
+clientsImmatriculations <- inner_join(clients, immatriculations, by = "immatriculation")
+
+# Suppression des duplicates
+clientsImmatriculations <- clientsImmatriculations[!duplicated(clientsImmatriculations), ]
+
+# Enregistrement du fichier de resultats au format csv
+write.table(clientsImmatriculations, file = "ClientsImmatriculations.csv", sep = ",", dec = ".", row.names = F)
